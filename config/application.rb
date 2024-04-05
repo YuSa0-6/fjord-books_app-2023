fd87ee254ada7096a049537f0bb13d2810b306d6
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-05 10:21:08 +0900

    ページネーションの日本語対応のために設定する

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb, yml}')]
+    I18n.available_locales = [:en, :ja]
+    I18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
