fc26f74a33ae30ebb98ae64fdd4fb0f09876d16a
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-08 11:45:12 +0900

    deviseでのusersに関するコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' 
+  }
   root to: 'books#index'
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
