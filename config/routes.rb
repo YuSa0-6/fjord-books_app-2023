72afbcb988dda1e416381e92e144de61fcdc1d0e
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-09 10:57:41 +0900

    users#index,#showのルーティングを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations' 
   }
-  root to: 'books#index'
   resources :books
+  resources :users, :only => [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
