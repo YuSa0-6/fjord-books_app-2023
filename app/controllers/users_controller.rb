ae408690469f3e132ab5f53b3502f57144c93be1
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-09 15:25:51 +0900

    rubocopを通す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def index
     @users = User.order(:id).page(params[:page]).per(2)
   end
