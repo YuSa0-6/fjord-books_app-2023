b47e1b27b258b4593c7649b1cdac0f9d840e04e6
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-09 15:04:53 +0900

    ブラウザ上でメール確認するためにletter_opener_webの設定をする

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   root to: 'books#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations' 
+    registrations: 'users/registrations' ,
+    passwords: 'users/passwords'
   }
   resources :books
   resources :users, :only => [:index, :show]
+
+  if Rails.env.development?
+    mount LetterOpener::Engine, at: "/letter_opener"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
