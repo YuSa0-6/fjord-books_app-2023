6a4bb884a30e4cc3121e407a0c2f18255f564ba3
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-09 10:50:55 +0900

    ユーザー情報を編集するためにストロングパラメターを設定する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code address introduction])
   end
 end
