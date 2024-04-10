66f0ec94503abedb898d21c937cfcae719e2980d
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-10 16:40:12 +0900

    mini_magickを導入してresizeするようにした。index,showに関しては事前にvariantを使用しているので元に戻した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [450, 300]
+    attachable.variant :max, resize_to_limit: [600, 700]
+  end
 end
