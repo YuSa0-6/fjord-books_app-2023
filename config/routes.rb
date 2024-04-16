1836c7d702b95d036d7b5f0134499bf308f2707c
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-16 16:25:58 +0900

    いらないものを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: %i[create destroy edit update], module: :books
+    resources :comments, only: %i[create destroy], module: :books
   end
   resources :reports do
-    resources :comments, only: %i[create destroy edit update], module: :reports
+    resources :comments, only: %i[create destroy], module: :reports
   end
   resources :users, only: %i(index show)
 end
