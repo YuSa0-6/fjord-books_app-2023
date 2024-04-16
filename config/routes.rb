37c292f137731f9b8a12c4d0d2d6a368cf4a9b82
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-16 09:54:56 +0900

    controllerとViewの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reports/new'
+  get 'reports/index'
+  get 'reports/show'
+  get 'reports/create'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
