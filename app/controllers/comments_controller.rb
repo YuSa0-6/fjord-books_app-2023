c1ef577d4e4c9aadccba3fbd40d6d043e3991edc
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-16 15:33:33 +0900

    comments_controllerを追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+
+  def edit; end
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      @comments = @commentable.comments
+      render_commentable_show
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
