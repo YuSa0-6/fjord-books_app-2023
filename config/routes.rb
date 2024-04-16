28293e62d6e2e96cb82da3c6c469bddaaa4979df
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-16 10:06:49 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'reports/new'
-  get 'reports/index'
-  get 'reports/show'
-  get 'reports/create'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :reports
 end
