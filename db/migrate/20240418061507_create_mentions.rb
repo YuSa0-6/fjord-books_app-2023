4e238ad5ba7eebbf3fe331fab684ce2c22f85556
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-22 11:28:51 +0900

    mentionsテーブルを定義。indexを使用してmentionテーブルの参照先ID、参照元IDの組み合わせが一意になるようにした。

--- db/migrate/20240418061507_create_mentions.rb
+++ db/migrate/20240418061507_create_mentions.rb
@@ -1,8 +1,13 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-
+      t.integer :mentioning_report_id, null:false
+      t.integer :mentioned_report_id,  null:false
       t.timestamps
+
+      t.index :mentioning_report_id
+      t.index :mentioned_report_id
+      t.index [:mentioning_report_id, :mentioned_report_id], unique: true
     end
   end
 end
