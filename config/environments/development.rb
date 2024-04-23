4ff90f7e4843cdcde2d0218f0c8a51a1001965cf
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-23 17:13:26 +0900

    コメント削除する

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  # この時点で、コンソールにいくつかの指示が表示されます。これらの手順の中で、各環境で Devise メーラーのデフォルトの URL オプションを設定する必要があります。可能な構成は次のとおりです
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
