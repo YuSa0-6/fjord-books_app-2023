7b106649963e61881beb0caf267152b2194dbe35
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-26 15:08:16 +0900

    test:　Userモデルの`name_or_email`の正常家テストを行うために、ユーザーに名前がない場合はemailを返すようにテストを実装した。

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'test9@sample.com', name: '')
+
+    assert_equal 'test9@sample.com', user.name_or_email
+  end
 end
