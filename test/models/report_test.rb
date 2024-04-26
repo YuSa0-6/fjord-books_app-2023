2d2575588bfc1914d2ac9fdbe121b921b15ca705
YuSa0-6 <fuku.sugar@icloud.com>
2024-04-26 16:07:20 +0900

    テスト　その日報が編集可能かどうかの返り値をテストするために、ユーザー、日報を作成し、trueを正常とするテストを書く。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,4 +6,9 @@ class ReportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test '#editable?' do
+    user = User.new(email: 'test@sample.com', password: 'password')
+    report = user.reports.new(title: 'title', content: 'content')
+    assert_equal true, report.editable?(user)
+  end
 end
