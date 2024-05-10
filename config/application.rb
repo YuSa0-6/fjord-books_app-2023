40387f8a6218cb8c9b337630a7d7bc192f3ddcff
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-10 19:26:40 +0900

    mini_magickからvipsに戻す。

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_storage.variant_processor = :mini_magick
+    config.active_storage.variant_processor = :vips
   end
 end
