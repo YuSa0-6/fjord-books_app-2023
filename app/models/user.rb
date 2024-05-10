2dd1a2c3737d440a44bdf29b2820c537992b3328
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-10 19:25:58 +0900

    結局使用していなかったので削除します。目的としてはリサイズ用の設定を事前に設定できることでしたが、使用しませんでした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [450, 300]
-    attachable.variant :max, resize_to_limit: [600, 700]
-  end
+  has_one_attached :avatar
 end
