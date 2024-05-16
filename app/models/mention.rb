f433cd34e74d57d51f1842d150bc15b206d879f5
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-16 11:34:41 +0900

    [review] https://github.com/YuSa0-6/fjord-books_app-2023/pull/6#discussion_r1582615230 可読性に影響がないのでコメントを削除する。

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Mention < ApplicationRecord
-  # この言及元(mentioned_reports)は日報に対して一つだから
   belongs_to :mentioned_report, class_name: 'Report', inverse_of: :mentioning_mentions
-  # この言及先(mentioning_reports)は日報に対して１つだから
   belongs_to :mentioning_report, class_name: 'Report', inverse_of: :mentioned_mentions
 
   validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
