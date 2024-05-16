633928dfcaae179d67996233391f88d8dd196a0d
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-16 11:50:41 +0900

    [review]https://github.com/YuSa0-6/fjord-books_app-2023/pull/6#discussion_r1582617346 rubocopの指示通りにinverse_ofを追記して、関連先のモデルを設定する。

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy # rubocop:disable Rails/InverseOf
+  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioned_reports, through: :mentioning_mentions, source: :mentioned_report
 
-  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy # rubocop:disable Rails/InverseOf
+  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioning_report
   has_many :mentioned_reports, through: :mentioned_mentions, source: :mentioning_report
 
   validates :title, presence: true
