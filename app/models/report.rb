82a3956e9382c871ec2fdec01c59e9e88f2ebef0
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-16 19:43:40 +0900

    関連付けでレコード登録できるようにする。

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,11 +4,13 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioning_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id,
+                                 dependent: :destroy
   has_many :mentioned_reports, through: :mentioning_mentions, source: :mentioned_report
 
-  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioning_report
-  has_many :mentioning_reports, through: :mentioned_mentions, source: :mentioning_report
+  has_many :mentioned_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id,
+                                dependent: :destroy
+  has_many :mentioning_reports, through: :mentioned_mentions, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -37,7 +39,8 @@ class Report < ApplicationRecord
     uri_ids.each do |uri_id|
       next unless other_report?(uri_id)
 
-      Mention.new(mentioned_report_id: uri_id, mentioning_report_id: id).save
+      # Mention.new(mentioned_report_id: uri_id, mentioning_report_id: id).save
+      mentioned_mentions.new(mentioning_report_id: uri_id).save!
     end
   end
 
