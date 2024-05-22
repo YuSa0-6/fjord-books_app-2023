3ecf7b9d26d72fc2eff45a30490ec4d7fdd8842f
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-22 11:50:15 +0900

    [review]https://github.com/YuSa0-6/fjord-books_app-2023/pull/6#discussion_r1605658553 - mention.rbのinverse_ofを削除する - report.rbの関連名を見直しする - save_mentoinsのメソッドを関連付けに合わせて変更する。

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Mention < ApplicationRecord
-  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :mentioning_mentions
-  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :mentioned_mentions
+  belongs_to :mentioned_report, class_name: 'Report'
+  belongs_to :mentioning_report, class_name: 'Report'
 
   validates :mentioning_report_id, uniqueness: { scope: :mentioned_report_id }
 end
