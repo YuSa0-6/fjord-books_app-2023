431f846ae32d5b59a8a6e56e8cb18f287d7d6c68
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-23 12:13:30 +0900

    [review]https://github.com/YuSa0-6/fjord-books_app-2023/pull/6#discussion_r1610823546 関連付けの名前を変更した。 Railsルール的に複数形のsで終わる方がトラブルを避けやすいらしい。

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,12 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentions_from, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy,
+  has_many :active_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy,
                            inverse_of: :mentioned_report
-  has_many :mentions_to, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy,
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy,
                          inverse_of: :mentioning_report
-  has_many :mentioning_reports, through: :mentions_from, source: :mentioning_report
-  has_many :mentioned_reports, through: :mentions_to, source: :mentioned_report
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioning_report
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
