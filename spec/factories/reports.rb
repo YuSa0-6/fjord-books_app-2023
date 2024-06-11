0ecb541f349840b4be1244bdf3e00d8e2b2458ea
YuSa0-6 <fuku.sugar@icloud.com>
2024-06-11 14:09:22 +0900

    report modelのテストを書く。

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :report do
     sequence(:title) { |n| "title#{n}" }
     sequence(:content) { |n| "content#{n}" }
+    created_at { Time.zone.now }
     association :user
   end
 end
